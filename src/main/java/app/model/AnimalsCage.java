07e7cfb9860c6d8b2f4d680adf5fac90e0cb3916
ValyaevRasim <jsm.group@mail.ru>
2022-01-25 20:46:41 +0200

    add Dog

---
+++ src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,25 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Component
+public class AnimalsCage {
+
+    @Autowired
+    @Qualifier("Dog")
+    private Animal animal;
+
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.println("At:");
+        System.out.println(new Timer().getTime());
+        System.out.println("________________________");
+    }
+}
